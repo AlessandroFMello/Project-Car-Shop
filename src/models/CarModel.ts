8c86bde60e31490e652317255ab332e2b316fc42
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-25 20:54:04 -0300

    feat: creates CarModel

---
+++ src/models/CarModel.ts
@@ -0,0 +1,28 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Car } from '../interfaces/CarInterface';
+import MongoModel from './MongoModel';
+
+interface CarDocument extends Car, Document { }
+
+const carSchema = new Schema<CarDocument>(
+  {
+    model: String,
+    year: Number,
+    color: String,
+    buyValue: Number,
+    seatsQty: Number,
+    doorsQty: Number,
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+class CarModel extends MongoModel<Car> {
+  constructor(model = createModel('Cars', carSchema)) {
+    super(model);
+  }
+}
+
+export default CarModel;
