d6d4f17518ccd8e7c93e65b96c75073d7fff2724
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-26 13:01:27 -0300

    feat; Creates MotorcycleInterface

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { Vehicle, vehicleSchema } from './VehicleInterface';
+
+const motorcycleSchema = vehicleSchema.extend({
+  category: z.string({
+    required_error: 'Category is required',
+    invalid_type_error: 'Category must be a string',
+  }) && z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number({
+    required_error: 'Engine capacity is required',
+    invalid_type_error: 'Engine capacity must be a number',
+  }).max(
+    2500,
+    { message: 'Engine capacity must be equal or less than 2500' },
+  ),
+  _id: z.optional(
+    z.string({ invalid_type_error: 'Id must be a string' })
+      .regex(
+        /^[0-9a-fA-F]{24}$/,
+        { message: 'Id must have 24 hexadecimal characters' },
+      ),
+  ),
+});
+
+export type Motorcycle = Vehicle & z.infer<typeof motorcycleSchema>;
