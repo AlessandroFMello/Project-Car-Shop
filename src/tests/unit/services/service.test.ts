d75980bcae8559f4bcfa64db6bbcf48b394a431c
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-27 20:18:18 -0300

    feat: Adds mocks helpers

--- src/tests/unit/services/service.test.ts
+++ src/tests/unit/services/service.test.ts
@@ -1,69 +1,12 @@
 import { expect } from 'chai';
-// import mongoose from 'mongoose';
 import Sinon from 'sinon';
 import { Car } from '../../../interfaces/CarInterface';
 import CarModel from '../../../models/CarModel';
 import CarsService from '../../../services/CarsService';
+import { allCarsMock, oneCarMock, newCarMock } from '../helpers/carMocks'
 
-const allCarsMock = [
-	{
-		"_id": "628f7b9ee2be6772f5e86099",
-		"model": "Marea Turno",
-		"year": 1995,
-		"color": "Black",
-		"buyValue": 17000,
-		"seatsQty": 4,
-		"doorsQty": 4
-	},
-	{
-		"_id": "628f7ba0e2be6772f5e8609b",
-		"model": "Marea Turno",
-		"year": 1995,
-		"color": "Black",
-		"buyValue": 17000,
-		"seatsQty": 4,
-		"doorsQty": 4
-	},
-	{
-		"_id": "628f7ba1e2be6772f5e8609d",
-		"model": "Marea Turno",
-		"year": 1995,
-		"color": "Black",
-		"buyValue": 17000,
-		"seatsQty": 4,
-		"doorsQty": 4
-	},
-	{
-		"_id": "628f7ba1e2be6772f5e8609f",
-		"model": "Marea Turno",
-		"year": 1995,
-		"color": "Black",
-		"buyValue": 17000,
-		"seatsQty": 4,
-		"doorsQty": 4
-	}
-]
 
-const newCarMock = {
-  "model": "Marea Turno",
-  "year": 1995,
-  "color": "Black",
-  "buyValue": 17000,
-  "seatsQty": 4,
-  "doorsQty": 4
-}
-
-const oneCarMock = {
-  "_id": "628fafea3b94a2894ec3f9e7",
-  "model": "Marea Turno",
-  "year": 1995,
-  "color": "Black",
-  "buyValue": 17000,
-  "seatsQty": 4,
-  "doorsQty": 4
-}
-
-describe('Testa CarModel', () => {
+describe('Testa CarServices', () => {
   let carModel = new CarModel();
   let carService = new CarsService(carModel);
 
