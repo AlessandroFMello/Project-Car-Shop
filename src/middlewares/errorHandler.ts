dee7d50a141e427f4d9207370d4eae787c22468f
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-28 06:28:18 -0300

    feat: creates errorMiddleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+const errorMiddleware = (
+  err: unknown,
+  _req: Request,
+  res: Response,
+) => {
+  console.log(err);
+
+  return res
+    .status(500)
+    .json({ err, error: 'Internal Server Error' });
+};
+
+export default errorMiddleware;
